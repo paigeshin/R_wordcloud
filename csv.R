84e934d11b2ac77c33a5a82763408354cb693b2d
paige <paige@paiges-Mac-mini.local>
2021-12-18 18:46:39 +0900

    update

--- csv.R
+++ csv.R
@@ -1,41 +1,60 @@
-setwd("~/Desktop/Math/Programming Methods/chicago")
+# devtools::install_github("lchiffon/wordcloud2")
 
-#libraries
-library(data.table) #fread
-library(dplyr) #ranking frequency
-## 
-## Attaching package: 'dplyr'
-## The following objects are masked from 'package:data.table':
-## 
-##     between, first, last
-## The following objects are masked from 'package:stats':
-## 
-##     filter, lag
-## The following objects are masked from 'package:base':
-## 
-##     intersect, setdiff, setequal, union
-library(grDevices) #png function
-library(wordcloud) #wordcloud
-## Loading required package: RColorBrewer
-library(RColorBrewer) #brewer colors
+# install.packages("wordcloud2")
+# install.packages("tm")
+# install.packages("readr")
+# install.packages("dplyr")
 
-#read in data table
-chi_dat = fread("chicago_6mos_google_communities.csv")
+library(wordcloud2)
+library(tm)
+library(readr)
+library(dplyr)
 
-#convert to data table
-chi_dat = as.data.table(chi_dat)
+# https://www.kaggle.com/dorianlazar/medium-articles-dataset?select=medium_data.csv
+# Read CSV and filter data with UX Collective Column
+medium_csv = read_csv("/Users/paige/Desktop/WordCloud/data/medium_data.csv") %>%
+  filter(publication == 'UX Collective')
 
-#table of total incidents by community
-#filter places outside of chicago communities
-chi_tab = chi_dat[community!=""]
-chi_tab2 = chi_tab[,.N, by=community]
+# Create Corpus
+medium.corpus = Corpus(VectorSource(medium_csv$title))
 
-#70 communities with gun violence incidents, ranked
-chi_table1 = chi_tab2[order(-N)] %>% head(70) %>% rename("Incidents" = N, "Community" = "community")
+removeHTML = function(text){
+  text = gsub(pattern = '<.+\\">', '', text)
+  text = gsub(pattern = '</.+>', '', text)
+  return(text)
+}
 
-#wordcloud
-png("wordcloud.png", width=12,height=8, units='in', res=300)
-par(mar = rep(0, 4))
-set.seed(1337)
-wordcloud(words = chi_table1$Community, freq = chi_table1$Incidents,scale=c(3.5,0.25),
-          max.words=70, colors=brewer.pal(8, "Dark2"))
+medium.corpus = medium.corpus %>%
+  tm_map(content_transformer(removeHTML)) %>%
+  tm_map(removeNumbers) %>%
+  tm_map(removePunctuation) %>%
+  tm_map(stripWhitespace) %>%
+  tm_map(content_transformer(tolower))  %>%
+  tm_map(removeWords, stopwords("english")) # %>%
+#  tm_map(removeWords, stopwords("SMART"))
+# Remove Some words
+# corpus <- tm_map(corpus, removeWords, c('get', 'told', 'gave'))
+
+tdm = TermDocumentMatrix(medium.corpus) %>%
+  as.matrix()
+words = sort(rowSums(tdm), decreasing = TRUE)
+df = data.frame(word = names(words), freq = words)
+
+df = df %>%
+  filter(nchar(as.character(word)) > 2,
+         word != "don’")
+
+uxc.colors = c("#fefefe", "#f4f2a8", "#030303")
+uxc.background = "#00ccff"
+
+library(extrafont)
+# font_import()
+fonts()
+
+wordcloud2(df,
+           color = rep_len(uxc.colors, nrow(df)),
+           backgroundColor = uxc.background,
+           fontFamily = "DM Sans",
+           size = 2.5,
+           minSize = 5,
+           rotateRatio = 0)
